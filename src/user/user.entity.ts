4e09d1316d621d87f9c5de82d1bbb6dd4f7522b9
omateusamaral <mateus.amaral018@gmail.com>
2022-02-19 17:59:01 -0300

    feat: created user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,35 @@
+import { encriptPassword } from '../utils/bcrypt-password';
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await encriptPassword(this.password);
+  }
+}
